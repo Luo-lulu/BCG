9065cb7595197375bd31fe8fbe1e70e2b38d2f35
Lulu.l <Lulu.l@LGroup.global>
2020-12-25 13:54:20 +0800

    add BCG SortStream content

--- src/component/SortStreams.js
+++ src/component/SortStreams.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React,{useState} from "react";
+import { Row, Col } from "antd";
+function SortStreams({ location, match },props) {
+  const [item,setItem]=useState([])
+  //console.log(match.params.id);
+  //console.log(location);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  
 
-function SortStreams({ location, match }) {
-  console.log(match.params.id);
-  console.log(location);
-  return <h1> {match.params.id} </h1>;
+  }
+
+
+  return(
+    <div>
+    <Row justify="center">
+      <Col>
+      <h1>這是 {match.params.id} 的分類表</h1>
+      </Col>
+    </Row>
+
+    <Row justify="center">
+    <Col>請輸入基本資料 :</Col>
+    </Row>
+
+    <Row justify="center">
+      <form onSubmit={handleSubmit}>
+      <Row>
+<Col  className="item__input">產品名: <input/></Col>
+<Col  className="item__input">銷售比: <input/></Col>
+<Col  className="item__input">利潤率: <input/></Col>
+<button>新增</button>
+</Row>
+</form>
+    </Row>
+
+    </div>
+  ) 
 }
 
 export default SortStreams;
